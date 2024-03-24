30e1ff4a0397bdf2bc406a6dfa9405859dda48ab
oraclexudehua <2320838977@qq.com>
2024-03-24 09:34:31 +0800

    update

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
     // base:'/oraclexudehua.github.io/brain-web/dist',
-  plugins: [react()],
+    build: { outDir: './docs' },
+    plugins: [react()],
 })
